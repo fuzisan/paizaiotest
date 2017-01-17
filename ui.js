7b4140ba2c630202eccfbc99fc60dfa56bc62a2b
toshichan <Wals@example.com>
2017-01-17 21:05:03 +0900

    ace editorなどui周りのjs

---
+++ ui.js
@@ -0,0 +1,24 @@
+var aceEditor = ace.edit("source_code");
+
+$("#run_button").on("click",function(event){
+  runCode();
+});
+aceEditor.commands.addCommand({
+  bindKey:{win: "Ctrl-Enter",mac:"Ctrl-Enter"},
+  exec:runCode,
+})
+
+function setEditorLanguage(language){
+  var languageToMode={
+    ruby:"ruby",
+    python: "python",
+    c:"c_cpp",
+  };
+  var mode = languageToMode[language];
+  aceEditor.getSession().setMode("ace/mode/"+mode);
+}
+$("#language").val("ruby");
+setEditorLanguage("ruby");
+$("#language").on("change",function(event){
+  setEditorLanguage(this.value);
+});
